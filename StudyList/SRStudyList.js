4bcd243dafb726d3cd3e4863280a886d1ee38583
Martin Nguyen <martin@maddin.io>
2017-07-15 15:02:35 +0200

    Added right header button to navigation bar.

--- StudyList/SRStudyList.js
+++ StudyList/SRStudyList.js
@@ -3,14 +3,23 @@
 import React from 'react';
 import {
   AppRegistry,
+  Button,
   Text,
   View
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 export default class SRStudyList extends React.Component {
-  static navigationOptions = {
-    title: 'Study List',
+  static navigationOptions = ({navigation}) => {
+    return {
+      title: 'Study List',
+      headerRight: (
+        <Button
+          title='+'
+          onPress={() => ({})}
+        />
+      ),
+    };
   };
 
   render() {
