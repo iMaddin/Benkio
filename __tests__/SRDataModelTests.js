a4b1b83caa384394a9d815137c74f8e90f6c0f83
Martin Nguyen <martin@maddin.io>
2017-07-17 22:10:58 +0200

    Added SRDataModelTests. Deleted test files because they failed for some reason. Updated package.json when attempting to fix jest problems.

---
+++ __tests__/SRDataModelTests.js
@@ -0,0 +1,10 @@
+// @flow
+import { reducer } from '../js/dataModel/SRSimpleDataModel'
+
+function sum(a, b) {
+  return a + b;
+}
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
