9ca2771c1ac073634a31996491934a5696207908
Martin Nguyen <martin@maddin.io>
2017-07-18 12:53:59 +0200

    Tasks now get grouped by their date string.

--- js/dataModel/SRDataPresenter.js
+++ js/dataModel/SRDataPresenter.js
@@ -1,9 +1,58 @@
 // @flow
 
 export const processDataForList = (data: object) => {
-  // TODO: turn data into something that can be put into SectionList
-  return [
-    {title: 'D', data: data},
-    {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-  ]
+  const { studyTasks } = data;
+
+  // get task name, (possibly notes under task name) and date
+  const taskNameAndDateArray = studyTasks.map(taskNameAndDate);
+
+  // group tasks with same date
+  const tasksGroupedByDate = groupTasksByDate(taskNameAndDateArray);
+
+  // sort by date
+  const sortedByDateArray = tasksGroupedByDate;
+  console.log(sortedByDateArray);
+  return sortedByDateArray
+}
+
+const taskNameAndDate = (singleTask) => {
+    const { taskName, dates } = singleTask
+    return {
+      "taskName": taskName, "nextDate": dates[0]
+    }
+}
+
+const addTaskNameToArrayMatchingDate = (array: Array<object>, date: string, taskNameToAdd: string) => {
+  const copyArray = [...array];
+  var returnArray = []
+
+  // search if resultArray already has object with that date
+  for (var i = 0; i < array.length; i++) {
+    const singleTask = copyArray[i];
+    const { nextDate, taskNames } = singleTask;
+
+    if(nextDate == date) {
+      singleTask['taskNames'] = [...taskNames, taskNameToAdd];
+      returnArray = copyArray;
+    } else {
+      returnArray = [...array, {nextDate: date, taskNames: [taskNameToAdd]}];
+    }
+  }
+
+  if (array.length == 0) {
+    returnArray = [...array, {nextDate: date, taskNames: [taskNameToAdd]}];
+  }
+
+  return returnArray;
+}
+
+const groupTasksByDate = (taskWithDatesArray: {taskName: string, nextDate: string}) => {
+  var resultArray = []; //{nextDate: string, taskNames: Array<string>}
+
+  for (var i=0; i < taskWithDatesArray.length; i++) {
+    const { taskName, nextDate } = taskWithDatesArray[i]
+    resultArray = addTaskNameToArrayMatchingDate(resultArray, nextDate, taskName);
+  }
+
+  return resultArray;
 }
