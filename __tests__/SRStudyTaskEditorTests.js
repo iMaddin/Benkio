5c275b7bf58685ad17fda67e7e0a36aabab2b933
Martin Nguyen <martin@maddin.io>
2017-07-22 14:33:52 +0200

    Added SRStudyTaskEditorTests.

---
+++ __tests__/SRStudyTaskEditorTests.js
@@ -0,0 +1,26 @@
+// @flow
+import SRStudyTaskEditor from '../js/SRStudyTaskEditor'
+
+test('input title label visibility', () => {
+  // hidden at launch when not readonly
+  // hidden when place text input placeholder is still visible
+})
+
+test('Added SRSpacedRepetition properties', () => {
+  // expect(.easinessFactor).toBeCloseTo(2.5)
+  // expect(.interval).toBe(1)
+  // expect(.repetition).toBe(0)
+})
+
+test('Check default edit UI elements properties', () => {
+  //  when editable: default selected date, intesity
+  //  when readonly: not editable, edit button, delete button, visible rating history
+})
+
+test('Custom selected date', () => {
+
+})
+
+test('', () => {
+
+})
