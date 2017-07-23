f309aa5bc9642e4caf072c3fe94b7e74b9b96dff
Martin Nguyen <martin@maddin.io>
2017-07-23 11:31:16 +0200

    Updated SRDataPresenter to pass more item properties.

--- js/dataModel/SRDataPresenter.js
+++ js/dataModel/SRDataPresenter.js
@@ -4,107 +4,111 @@ import { SRSpacedRepetition } from '../SRSpacedRepetition'
 import moment from 'moment'
 import expect, { createSpy, spyOn, isSpy } from 'expect'
 
+//Array<{title: string, data: string}>
 export const processDataForList = (studyTasks) => {
   /**
-   * 1. get only information needed for section list, in this case task name and the date
+   * 1. take object that has _at least_ the properties we need { id, taskName, notes, dates, srs }
    * 2. Group tasks that belong to the same day
    * 3. sort day groups
    * 4. sort tasks inside day groups
    */
-  const taskNameAndDateArray = studyTasks.map(taskNameAndDate)
-  const tasksGroupedByDate = groupTasksByDate(taskNameAndDateArray)
-  const sortedByDateArray = sortByDate(tasksGroupedByDate)
+  const itemArray = studyTasks.map(itemWithCalculatedDueDate)
+  const itemsGroupedByDate = groupItemsByDate(itemArray)
+  const sortedByDateArray = sortByDate(itemsGroupedByDate)
   const listReadyArray = prepareArrayForSectionList(sortedByDateArray)
   return listReadyArray
 }
 
-const taskNameAndDate = (singleTask) => {
-    const { taskName, dates, srs } = singleTask
+const itemWithCalculatedDueDate = (singleTask) => {
+    const { id, taskName, notes, dates, srs } = singleTask
     const { easinessFactor, interval, repetition } = srs
     const srsData = new SRSpacedRepetition(easinessFactor, interval, repetition)
     const nextDueDate = srsData.nextDate(dates[0])
     return {
-      "taskName": taskName, "nextDate": nextDueDate
+      id: id,
+      taskName: taskName,
+      notes: notes,
+      date: nextDueDate
     }
 }
 
-// TODO: rename to addTaskToArrayMatchingDate
-// TODO: change parameter to task: {date: string, taskName: string}
-const addTaskNameToArrayMatchingDate = (array: Array<object>, date: string, taskNameToAdd: string) => {
-  var returnArray = [...array]
-  const dateObject = new Date(date)
-  var foundExistingDateGroup = false
-
-  // search if resultArray already has object with that date
-  array.forEach((item) => {
-    const singleTask = item
-    const { dateWithoutTime, tasksWithTimes } = singleTask
-    const dateWithoutTimeObject = new Date(dateWithoutTime)
-
-    // compare dates, if dates match the day, then group
-    if(dateWithoutTimeObject.toDateString() == dateObject.toDateString()) { // adds new tasks to existing date group
-      singleTask['tasksWithTimes'] = [...tasksWithTimes, {taskName: taskNameToAdd, taskDate: date}]
-      foundExistingDateGroup = true
-      return
-    }
-  })
-
-  if (returnArray.length == 0 || !foundExistingDateGroup) {
-    returnArray = [...returnArray, {dateWithoutTime: dateObject.toDateString(), tasksWithTimes: [{taskName: taskNameToAdd, taskDate: date}]}]
-  }
-
-  return returnArray
-}
-
-const groupTasksByDate = (taskWithDatesArray) => {
+const groupItemsByDate = (itemArray) => {
   var resultArray = []
 
-  taskWithDatesArray.forEach((item) => {
-    const { taskName, nextDate } = item
-    resultArray = addTaskNameToArrayMatchingDate(resultArray, nextDate, taskName)
+  itemArray.forEach((item) => {
+    const { id, taskName, notes, date } = item
+    resultArray = addItemToArrayMatchingDate(resultArray, item)
   })
 
   return resultArray
 }
 
-const sortByDate = (tasksGroupedByDateArray) => {
-  const sortedByDateGroupArray = sortDateGroup(tasksGroupedByDateArray)
+const addItemToArrayMatchingDate = (array, item) => {
+  var arrayCopy = [...array]
+  const { id, taskName, notes, date } = item
+  const dateObject = new Date(date)
+  var foundExistingDateGroup = false
+
+  // search if resultArray already has object with that date
+  arrayCopy.forEach((section) => {
+    const { dateGroup, items } = section
+    const dateWithoutTimeObject = new Date(dateGroup)
+
+    // compare dates, if dates match the day, then group
+    if(dateWithoutTimeObject.toDateString() == dateObject.toDateString()) { // adds new tasks to existing date group
+      section.items = [...items, {id: id, taskName: taskName, notes: notes, date: date}]
+      foundExistingDateGroup = true
+      return
+    }
+  })
+
+  // creates new date group
+  if (arrayCopy.length == 0 || !foundExistingDateGroup) {
+    arrayCopy = [...arrayCopy, {
+      dateGroup: dateObject.toDateString(),
+      items: [{id: id, taskName: taskName, notes: notes, date: date}]
+    }]
+  }
+
+  return arrayCopy
+}
+
+const sortByDate = (itemsGroupedByDateArray) => {
+  const sortedByDateGroupArray = sortDateGroup(itemsGroupedByDateArray)
   const sortedTasksByDateArray = sortTasksByDate(sortedByDateGroupArray)
   return sortedTasksByDateArray
 }
 
-const sortDateGroup = (array: Array<{dateWithoutTime: string, tasksWithTimes: Array<{taskName: string, taskDate: string}>}>) => {
+const sortDateGroup = (array) => {
   return array.sort((a,b) => {
-    return new Date(a.dateWithoutTime) - new Date(b.dateWithoutTime)
+    return new Date(a.dateGroup) - new Date(b.dateGroup)
   })
 }
 
-const sortTasksByDate = (array: Array<{dateWithoutTime: string, tasksWithTimes: Array<{taskName: string, taskDate: string}>}>) => {
+const sortTasksByDate = (array) => {
   var arrayCopy = [...array]
 
-  arrayCopy.forEach((item) => {
-    const { tasksWithTimes } = item
-    arrayCopy['tasksWithTimes'] = tasksWithTimes.sort((a,b) => {
-      return new Date(a.taskDate) - new Date(b.taskDate) // earlier time first
+  arrayCopy.forEach((section) => {
+    const { items } = section
+    arrayCopy.items = items.sort((a,b) => {
+      return new Date(a.date) - new Date(b.date) // earlier time first
     })
   })
 
   return arrayCopy
 }
 
-//Array<{title: string, data: string}>
-const prepareArrayForSectionList = (array: Array<{dateWithoutTime: string, tasksWithTimes: Array<{taskName: string, taskDate: string}>}>) => {
-
+const prepareArrayForSectionList = (array) => {
   var resultArray = []
 
-  array.forEach((item) => {
-    const { dateWithoutTime, tasksWithTimes } = item
-    const formattedTitle = formatDateForTitle(dateWithoutTime)
+  array.forEach((section) => {
+    const { dateGroup, items } = section
+    const formattedTitle = formatDateForTitle(dateGroup)
 
     var dataArray = []
-    tasksWithTimes.forEach((taskItem) => {
-      const { taskName } = taskItem
-      dataArray = [...dataArray, taskName]
+    items.forEach((taskItem) => {
+      const { id, taskName, notes } = taskItem
+      dataArray = [...dataArray, {id: id, taskName: taskName, notes: notes}]
     })
 
     resultArray = [...resultArray, {title: formattedTitle, data: dataArray}]
@@ -141,25 +145,62 @@ const testTaskNameAndDate = () => {
     id: '5srtvsvev',
     taskName: 'ませんか',
     notes: 'efefasdad',
-    nextDate: new Date('July 17, 2017 11:13:00').toString()
+    date: new Date('July 17, 2017 11:13:00').toString()
   }
 
-  expect(taskNameAndDate(beforeState)).toEqual(expectedState)
+  expect(itemWithCalculatedDueDate(beforeState)).toEqual(expectedState)
 }
 testTaskNameAndDate()
 
+const testGroupItemsByDate = () => {
+  const beforeState = [
+    { "id": "5srtvsvev",
+      "taskName": "ませんか",
+      "notes": "",
+      "date": "July 17, 2017 11:13:00",
+    },
+    { "id": "vtsrvtrtv",
+      "taskName": "ましょう・ましょうか",
+      "notes": "",
+      "date": "July 17, 2017 11:13:00",
+    },
+    { "id": "jdb56srv5",
+      "taskName": "てはいけません",
+      "notes": "",
+      "date": "July 16, 2017 10:13:00",
+    },
+    { "id": "S7SM6ND6BVD",
+      "taskName": "何も",
+      "notes": "",
+      "date": "July 16, 2017 11:13:00",
+    },
+    { "id": "D6UD6MU",
+      "taskName": "どこかに・どこにも",
+      "notes": "",
+      "date": "July 18, 2017 11:13:00",
+    },
+  ]
+
+  const expectedState = [
+
+  ]
+
+  expect(groupItemsByDate(beforeState)).toEqual(expectedState)
+}
+// testGroupItemsByDate()
+
 const testSortDateGroup = () => {
   const beforeState = [
-    {dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: []},
-    {dateWithoutTime: 'Sat Jul 29 2017', tasksWithTimes: []},
-    {dateWithoutTime: 'Tue Jul 25 2017', tasksWithTimes: []},
-    {dateWithoutTime: 'Thu Jul 20 2017', tasksWithTimes: []}
+    {dateGroup: 'Wed Jul 19 2017', items: []},
+    {dateGroup: 'Sat Jul 29 2017', items: []},
+    {dateGroup: 'Tue Jul 25 2017', items: []},
+    {dateGroup: 'Thu Jul 20 2017', items: []}
   ]
   const expectedState = [
-    {dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: []},
-    {dateWithoutTime: 'Thu Jul 20 2017', tasksWithTimes: []},
-    {dateWithoutTime: 'Tue Jul 25 2017', tasksWithTimes: []},
-    {dateWithoutTime: 'Sat Jul 29 2017', tasksWithTimes: []},
+    {dateGroup: 'Wed Jul 19 2017', items: []},
+    {dateGroup: 'Thu Jul 20 2017', items: []},
+    {dateGroup: 'Tue Jul 25 2017', items: []},
+    {dateGroup: 'Sat Jul 29 2017', items: []},
   ]
   expect(sortDateGroup(beforeState)).toEqual(expectedState)
 }
@@ -167,27 +208,27 @@ testSortDateGroup()
 
 const testSortTasksByDate = () => {
   const beforeState = [
-    {dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: [
-      {taskName: 'san', taskDate: 'July 19, 2017 11:14:00'},
-      {taskName: 'ni', taskDate: 'July 19, 2017 22:23:00'},
-      {taskName: 'ichi', taskDate: 'July 19, 2017 08:34:00'},
-      {taskName: 'yon', taskDate: 'July 19, 2017 17:35:00'}
+    {dateGroup: 'Wed Jul 19 2017', items: [
+      {taskName: 'san', date: 'July 19, 2017 11:14:00'},
+      {taskName: 'ni', date: 'July 19, 2017 22:23:00'},
+      {taskName: 'ichi', date: 'July 19, 2017 08:34:00'},
+      {taskName: 'yon', date: 'July 19, 2017 17:35:00'}
     ]},
-    {dateWithoutTime: 'Thu Jul 20 2017', tasksWithTimes: [
-      {taskName: 'go', taskDate: 'July 20, 2017 17:13:00'},
-      {taskName: 'roku', taskDate: 'July 20, 2017 11:13:00'}
+    {dateGroup: 'Thu Jul 20 2017', items: [
+      {taskName: 'go', date: 'July 20, 2017 17:13:00'},
+      {taskName: 'roku', date: 'July 20, 2017 11:13:00'}
     ]}
   ]
   const expectedState = [
-    {dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: [
-      {taskName: 'ichi', taskDate: 'July 19, 2017 08:34:00'},
-      {taskName: 'san', taskDate: 'July 19, 2017 11:14:00'},
-      {taskName: 'yon', taskDate: 'July 19, 2017 17:35:00'},
-      {taskName: 'ni', taskDate: 'July 19, 2017 22:23:00'}
+    {dateGroup: 'Wed Jul 19 2017', items: [
+      {taskName: 'ichi', date: 'July 19, 2017 08:34:00'},
+      {taskName: 'san', date: 'July 19, 2017 11:14:00'},
+      {taskName: 'yon', date: 'July 19, 2017 17:35:00'},
+      {taskName: 'ni', date: 'July 19, 2017 22:23:00'}
     ]},
-    {dateWithoutTime: 'Thu Jul 20 2017', tasksWithTimes: [
-      {taskName: 'roku', taskDate: 'July 20, 2017 11:13:00'},
-      {taskName: 'go', taskDate: 'July 20, 2017 17:13:00'}
+    {dateGroup: 'Thu Jul 20 2017', items: [
+      {taskName: 'roku', date: 'July 20, 2017 11:13:00'},
+      {taskName: 'go', date: 'July 20, 2017 17:13:00'}
     ]}
   ]
   expect(sortTasksByDate(beforeState)).toEqual(expectedState)
@@ -202,15 +243,15 @@ testFormatDateForTitle()
 
 const testPrepareArrayForSectionList = () => {
   const beforeState = [
-    {dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: [
-      {taskName: 'ichi', taskDate: 'July 19, 2017 08:34:00'},
-      {taskName: 'san', taskDate: 'July 19, 2017 11:14:00'},
-      {taskName: 'yon', taskDate: 'July 19, 2017 17:35:00'},
-      {taskName: 'ni', taskDate: 'July 19, 2017 22:23:00'}
+    {dateGroup: 'Wed Jul 19 2017', items: [
+      {taskName: 'ichi', date: 'July 19, 2017 08:34:00'},
+      {taskName: 'san', date: 'July 19, 2017 11:14:00'},
+      {taskName: 'yon', date: 'July 19, 2017 17:35:00'},
+      {taskName: 'ni', date: 'July 19, 2017 22:23:00'}
     ]},
-    {dateWithoutTime: 'Thu Jul 20 2017', tasksWithTimes: [
-      {taskName: 'roku', taskDate: 'July 20, 2017 11:13:00'},
-      {taskName: 'go', taskDate: 'July 20, 2017 17:13:00'}
+    {dateGroup: 'Thu Jul 20 2017', items: [
+      {taskName: 'roku', date: 'July 20, 2017 11:13:00'},
+      {taskName: 'go', date: 'July 20, 2017 17:13:00'}
     ]}
   ]
   const expectedState = [
@@ -227,58 +268,99 @@ const testPrepareArrayForSectionList = () => {
   ]
   expect(prepareArrayForSectionList(beforeState)).toEqual(expectedState)
 }
-testPrepareArrayForSectionList()
+// testPrepareArrayForSectionList()
 
 const testAddTaskNameToArrayMatchingDateWithEmptyArray = () => {
   const beforeState = []
-  const afterState = addTaskNameToArrayMatchingDate(beforeState, 'July 19, 2017 11:13:00', 'hello')
-  const expectedState = [{dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: [{taskName: 'hello', taskDate: 'July 19, 2017 11:13:00'}]}]
-  expect(afterState).toEqual(expectedState)
+  const item =  {
+    id: 'ss4bcrsc',
+    taskName: 'hello',
+    notes: 'TiV7A!a]x',
+    date: 'July 19, 2017 11:13:00'
+  }
+  const expectedState = [{
+    dateGroup: 'Wed Jul 19 2017',
+    items: [{
+      id: 'ss4bcrsc',
+      taskName: 'hello',
+      notes: 'TiV7A!a]x',
+      date: 'July 19, 2017 11:13:00'
+    }]
+  }]
+  expect(addItemToArrayMatchingDate(beforeState, item)).toEqual(expectedState)
 }
+testAddTaskNameToArrayMatchingDateWithEmptyArray()
+
 const testAddTaskNameToArrayMatchingDateWithDifferentDate = () => {
-  const beforeState = [{dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: [{taskName: 'hello', taskDate: 'July 19, 2017 11:13:00'}]}]
-  const afterState = addTaskNameToArrayMatchingDate(beforeState, 'July 20, 2017 11:13:00', 'bye')
-  const expectedState = [
-    {dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: [{taskName: 'hello', taskDate: 'July 19, 2017 11:13:00'}]},
-    {dateWithoutTime: 'Thu Jul 20 2017', tasksWithTimes: [{taskName: 'bye', taskDate: 'July 20, 2017 11:13:00'}]}
-  ]
-  expect(afterState).toEqual(expectedState)
+  const beforeState = [{
+    dateGroup: 'Wed Jul 19 2017',
+    items: [{
+      id: 'ss4bcrsc',
+      taskName: 'hello',
+      notes: 'TiV7A!a]x',
+      date: 'July 19, 2017 11:13:00'
+    }]
+  }]
+  const item = {
+    id: 'Q$Kv&6lvzFp%tAh',
+    taskName: 'Dv%^002T8',
+    notes: 'uY3!2&HGLg6dJ]rq*[uE',
+    date: 'July 25, 2017 11:13:00'
+  }
+  const expectedState = [{
+    dateGroup: 'Wed Jul 19 2017',
+    items: [{
+      id: 'ss4bcrsc',
+      taskName: 'hello',
+      notes: 'TiV7A!a]x',
+      date: 'July 19, 2017 11:13:00'
+    }]
+  },
+  {
+    dateGroup: 'Tue Jul 25 2017',
+    items: [{
+      id: 'Q$Kv&6lvzFp%tAh',
+      taskName: 'Dv%^002T8',
+      notes: 'uY3!2&HGLg6dJ]rq*[uE',
+      date: 'July 25, 2017 11:13:00'
+    }]
+  }]
+  expect(addItemToArrayMatchingDate(beforeState, item)).toEqual(expectedState)
 }
+testAddTaskNameToArrayMatchingDateWithDifferentDate()
+
 const testAddTaskNameToArrayMatchingDateWithSameDate = () => {
-  const beforeState = [{dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: [{taskName: 'hello', taskDate: 'July 19, 2017 11:13:00'}]}]
-  const afterState = addTaskNameToArrayMatchingDate(beforeState, 'July 19, 2017 13:13:00', 'hola')
+  const beforeState = [{dateGroup: 'Wed Jul 19 2017', items: [{taskName: 'hello', date: 'July 19, 2017 11:13:00'}]}]
+  const afterState = addItemToArrayMatchingDate(beforeState, 'July 19, 2017 13:13:00', 'hola')
   const expectedState = [
-    {dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes:
+    {dateGroup: 'Wed Jul 19 2017', items:
     [
-      {taskName: 'hello', taskDate: 'July 19, 2017 11:13:00'},
-      {taskName: 'hola', taskDate: 'July 19, 2017 13:13:00'}
+      {taskName: 'hello', date: 'July 19, 2017 11:13:00'},
+      {taskName: 'hola', date: 'July 19, 2017 13:13:00'}
     ]}]
   expect(afterState).toEqual(expectedState)
 }
+// testAddTaskNameToArrayMatchingDateWithSameDate()
 
 const testAddTaskNameToArrayMatchingDateWithSameDateAtEndOfArray = () => {
   const beforeState = [
-    {dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: [
-      {taskName: 'hello', taskDate: 'July 19, 2017 11:13:00'}
+    {dateGroup: 'Wed Jul 19 2017', items: [
+      {taskName: 'hello', date: 'July 19, 2017 11:13:00'}
     ]},
-    {dateWithoutTime: 'Fri Jul 20 2017', tasksWithTimes: [
-      {taskName: 'TGIF', taskDate: 'July 20, 2017 11:13:00'}
+    {dateGroup: 'Fri Jul 20 2017', items: [
+      {taskName: 'TGIF', date: 'July 20, 2017 11:13:00'}
     ]}
   ]
-  const afterState = addTaskNameToArrayMatchingDate(beforeState, 'July 20, 2017 13:13:00', 'Leeroy Jenkins')
+  const afterState = addItemToArrayMatchingDate(beforeState, 'July 20, 2017 13:13:00', 'Leeroy Jenkins')
   const expectedState = [
-    {dateWithoutTime: 'Wed Jul 19 2017', tasksWithTimes: [
-      {taskName: 'hello', taskDate: 'July 19, 2017 11:13:00'}
+    {dateGroup: 'Wed Jul 19 2017', items: [
+      {taskName: 'hello', date: 'July 19, 2017 11:13:00'}
     ]},
-    {dateWithoutTime: 'Fri Jul 20 2017', tasksWithTimes: [
-      {taskName: 'TGIF', taskDate: 'July 20, 2017 11:13:00'},
-      {taskName: 'Leeroy Jenkins', taskDate: 'July 20, 2017 13:13:00'}
+    {dateGroup: 'Fri Jul 20 2017', items: [
+      {taskName: 'TGIF', date: 'July 20, 2017 11:13:00'},
+      {taskName: 'Leeroy Jenkins', date: 'July 20, 2017 13:13:00'}
     ]}
   ]
   expect(afterState).toEqual(expectedState)
 }
-
-testAddTaskNameToArrayMatchingDateWithEmptyArray()
-testAddTaskNameToArrayMatchingDateWithDifferentDate()
-testAddTaskNameToArrayMatchingDateWithSameDate()
-testAddTaskNameToArrayMatchingDateWithSameDateAtEndOfArray()
+// testAddTaskNameToArrayMatchingDateWithSameDateAtEndOfArray()
