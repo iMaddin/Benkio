ee1af6c7e4558bf5b710f96ce3fc0e514afaff0b
Martin Nguyen <martin@maddin.io>
2017-07-27 16:40:53 +0200

    Fixed updating and rating tasks not working.

--- js/SRHome.js
+++ js/SRHome.js
@@ -8,6 +8,7 @@ import expect from 'expect'
 
 import SRStudyList, { studyListTitle } from './SRStudyList'
 import SRStudyTaskEditor from './SRStudyTaskEditor'
+import SRSpacedRepetition from './SRSpacedRepetition'
 import { SRDarkColor, SRYellowColor, SRBrightColor, SRRedColor } from './utilities/SRColors'
 import { mapDispatchToProps, mapStateToProps } from './dataModel/SRDataManipulator'
 
@@ -30,11 +31,47 @@ class SRHome extends Component {
 
         <SRStudyList
           navigationAction ={item => this.navigateToItem(item)}
-          rateAction = {(item, grade) => this.props.rateItem(item, grade)}
+          rateAction = {(item, grade) => this.rateTask(item, grade)}
         />
 
        </View>
     )
+  }
+
+  updateTask = (
+    newTask: {id: string, taskName: string, notes: string, date: string},
+    oldTask: {id: string, taskName: string, notes: string, date: string}) => {
+    const { replaceItem, studyTasks } = this.props
+    const mergedItem = this.dataWithID(oldTask.id, studyTasks)
+    mergedItem.taskName = newTask.taskName
+    mergedItem.notes = newTask.notes
+    replaceItem(mergedItem)
+  }
+
+  rateTask = (id: string, grade: number) => {
+    const { replaceItem, studyTasks } = this.props
+    expect(id).toExist('rateItem(): undefined id')
+    const item = this.dataWithID(id, studyTasks)
+
+    // update SRS, rating history, date rated,
+    item.ratingHistory.push(grade)
+
+    const dateRated = new Date().toString()
+    item.dates.push(dateRated)
+
+    const { easinessFactor, interval, repetition } = item.srs
+    const updatedSRS = new SRSpacedRepetition(easinessFactor, interval, repetition).grade(grade)
+    item.srs = updatedSRS
+
+    replaceItem(item)
+  }
+
+  dataWithID = (id: string, studyTasks: Array<Object>) => {
+    expect(id).toExist('dataWithID(): Undefined id')
+    const filteredArray = studyTasks.filter((item) => item.id == id)
+    expect(filteredArray.length).toBe(1, `Looking for data with id: ${id}. Item: ${JSON.stringify(studyTasks)}`)
+    const item = filteredArray[0]
+    return item
   }
 
   // SRStudyListCell
@@ -43,7 +80,7 @@ class SRHome extends Component {
     const displayProps = {
       readonly: true,
       item: item,
-      saveAction: (newItem, oldItem) => this.props.replaceItem(newItem, oldItem),
+      saveAction: (newItem, oldItem) => this.updateTask(newItem, oldItem),
       deleteAction: () => this.deleteTask(item),
     }
     this.props.navigation.navigate(StudyTaskDetailsScreenName, displayProps)
--- js/dataModel/SRDataManipulator.js
+++ js/dataModel/SRDataManipulator.js
@@ -1,9 +1,9 @@
 // @flow
+import { connect } from 'react-redux'
+
 import { actionCreators, SRStudyTask, SRStudyTaskIntensity } from './SRSimpleDataModel'
 import uuid from '../utilities/UUID'
 import SRSpacedRepetition from '../SRSpacedRepetition'
-import { connect } from 'react-redux'
-import expect from 'expect'
 
 const addTask = (task: {taskName: string, notes: string, date: string}) => {
   const { taskName, notes, date } = task
@@ -21,44 +21,7 @@ const addTask = (task: {taskName: string, notes: string, date: string}) => {
   return studyTask
 }
 
-const updateTask = (
-  newTask: {id: string, taskName: string, notes: string, date: string},
-  oldTask: {id: string, taskName: string, notes: string, date: string},
-  studyTasks: Array<Object>
-) => {
-  const mergedItem = dataWithID(oldTask.id, studyTasks)
-  mergedItem.taskName = newTask.taskName
-  mergedItem.notes = newTask.notes
-  return mergedItem
-}
-
-const rateTask = (id: string, grade: number, studyTasks: Array<Object>) => {
-  expect(id).toExist('rateItem(): undefined id')
-  const item = dataWithID(id, studyTasks)
-
-  // update SRS, rating history, date rated,
-  item.ratingHistory.push(grade)
-
-  const dateRated = new Date().toString()
-  item.dates.push(dateRated)
-
-  const { easinessFactor, interval, repetition } = item.srs
-  const updatedSRS = new SRSpacedRepetition(easinessFactor, interval, repetition).grade(grade)
-  item.srs = updatedSRS
-
-  return item
-}
-
-const dataWithID = (id: string, studyTasks: Array<Object>) => {
-  expect(id).toExist('dataWithID(): Undefined id')
-  const filteredArray = studyTasks.filter((item) => item.id == id)
-  expect(filteredArray.length).toBe(1, `Looking for data with id: ${id}. Item: ${JSON.stringify(studyTasks)}`)
-  const item = filteredArray[0]
-  return item
-}
-
-export const mapDispatchToProps = (dispatch, props) => {
-  const { studyTasks } = props
+export const mapDispatchToProps = dispatch => {
 
   return {
     addItem: item => {
@@ -68,14 +31,9 @@ export const mapDispatchToProps = (dispatch, props) => {
     removeItem: item => {
       dispatch(actionCreators.remove(item))
     },
-    replaceItem: (newTask, oldTask) => {
-      const updatedTask = updateTask(newTask, oldTask, studyTasks)
-      dispatch(actionCreators.replace(updatedTask))
+    replaceItem: item => {
+      dispatch(actionCreators.replace(item))
     },
-    rateItem: (id, grade) => {
-      const ratedTask = rateTask(id, grade, studyTasks)
-      dispatch(actionCreators.replace(ratedTask))
-    }
   }
 }
 
