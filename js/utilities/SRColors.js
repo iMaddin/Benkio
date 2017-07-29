1a8bc38764b97703be8c3645fe832c6357a41e4c
Martin Nguyen <martin@maddin.io>
2017-07-29 10:53:13 +0200

    Changed SRColor hex values with rgba.

--- js/utilities/SRColors.js
+++ js/utilities/SRColors.js
@@ -1,6 +1,6 @@
 // @flow
 
-export const SRDarkColor = '#393E41'
-export const SRYellowColor = '#FFFC31'
-export const SRBrightColor = '#F6F7EB'
-export const SRRedColor = '#E94F37'
+export const SRDarkColor = 'rgba(57, 62, 65, 1.00)'
+export const SRYellowColor = 'rgba(255, 252, 49, 1.00)'
+export const SRBrightColor = 'white'//'rgba(246, 247, 235, 1.00)'
+export const SRRedColor = 'rgba(233, 79, 55, 1.00)'
